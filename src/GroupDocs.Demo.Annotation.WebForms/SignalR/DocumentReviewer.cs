512b7925b625302fbb478043cabed0772e8abfcd
groupdocs <vitaliy.rezchikov@groupdocs.com>
2016-04-15 17:59:20 +0300

    Fixed reference to GroupDocs.Data.Json project;
    Code cleanup

--- src/GroupDocs.Demo.Annotation.WebForms/SignalR/DocumentReviewer.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GroupDocs.Demo.Annotation.WebForms.SignalR
-{
-    public struct DocumentReviewer
-    {
-        public string DocumentGuid { get; set; }
-        public string UserGuid { get; set; }
-    }
-}
